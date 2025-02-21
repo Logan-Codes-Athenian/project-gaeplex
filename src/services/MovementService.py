2eff930f80ab95d4cd94894283337b76a971b039
L McM <37253582+Papa-Athenian-Franku@users.noreply.github.com>
2025-02-21 12:13:01 +0000

    Update cav detection logic

--- src/services/MovementService.py
+++ src/services/MovementService.py
@@ -52,11 +52,12 @@ class MovementService:
             base_minutes_per_hex = 30
         else:
             army_units = movement.get("army", [])
+            army_units_split =  army_units.split(",| ") if army_units != [] else []
             cav_terms = {"cavalry", "cav", "upstart noble band", "frankish knights"}
             
             # Check if army is not empty and all elements are cavalry-related
             cav_only = bool(army_units) and all(
-                any(cav in unit.lower() for cav in cav_terms) for unit in army_units
+                any(cav in unit.lower() for cav in cav_terms) for unit in army_units_split
                 )
             
             if cav_only:
